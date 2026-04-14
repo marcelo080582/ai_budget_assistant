1ae6985c3a79e62353fddef85a689d9ac163181d
Marcelo Martins <marcelo080582@gmail.com>
2026-04-14 08:11:41 -0300

    correct variable names

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -2,19 +2,19 @@ class ChatController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-     ask = params[:message].to_s.downcase.strip
+     message = params[:message].to_s.downcase.strip
      
-     question =
-      if ask.include?("quantos orçamentos hoje")
+     answer =
+      if message.include?("quantos orçamentos hoje")
         total = Budget.where(created_at: Time.current.beginning_of_day..Time.current.end_of_day).count
         "Hoje foram encontrados #{total} orçamento(s)."
-      elsif ask.include?("qual o valor total dos orçamentos hoje")
+      elsif message.include?("qual o valor total dos orçamentos hoje")
         total = Budget.where(created_at: Time.current.beginning_of_day..Time.current.end_of_day).sum(:total_value)
         "O valor total dos orçamentos de hoje é R$ #{format('%.2f', total)}."
       else
         "Ainda não sei responder essa pergunta."
       end
 
-    render json: { answer: question }
+    render json: { answer: answer }
   end
 end
